6eabd4b1289d10be189cb49af6248ee57978ded7
irfan Rakib <irfanrakib756@gmail.com>
2025-11-10 23:35:49 +0600

    added navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { NavLink } from "react-router";
-
+import { Link, NavLink } from "react-router";
+import Logo1 from "./../../assets/Logo1.png";
+import Logo from "./../../assets/Logo.png";
 const NavBar = () => {
   const Links = (
     <>
@@ -44,7 +45,29 @@ const NavBar = () => {
               {Links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <Link
+            to="/"
+            className="btn flex gap-0 justify-center items-center py-7 px-3 text-xl"
+          >
+            <img
+              className="w-25 text-center block md:hidden"
+              src={Logo1}
+              alt="Logo1"
+            />
+            <img
+              className="w-35 pt-5 text-center hidden md:block"
+              src={Logo}
+              alt="Logo"
+            />
+            <div className="hidden md:block">
+              <h4 className="text-[#A88647] text-balance">
+                Study<span className="text-[#4A7BA8]">Mate</span>
+              </h4>
+              <p className="text-sm text-gray-500">
+                Unlock Your Learning Potential
+              </p>
+            </div>
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{Links}</ul>
